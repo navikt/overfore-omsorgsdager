74aef7e40831631cd9916f97a674cf2ec516ca5c
Frode Hansen <frode.hansen@bekk.no>
2020-03-17 09:59:10 +0100

    Delete mocks

--- __mocks__/axios.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    get: jest.fn(() => Promise.resolve({ data: {} })),
-    post: jest.fn(() => Promise.resolve({ data: {} })),
-    delete: jest.fn(() => Promise.resolve({ data: {} }))
-};
--- __mocks__/history.ts
+++
@@ -1,7 +0,0 @@
-export const historyMock = {
-    push: jest.fn(),
-    listen: jest.fn(),
-    location: {}
-};
-
-export const createMemoryHistory = () => historyMock;
