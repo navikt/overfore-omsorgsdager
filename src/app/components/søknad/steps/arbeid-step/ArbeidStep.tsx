5c8f08d25c261640cd681c760ca97ef1bac7eefa
Are Bjølseth <are.bjolseth@nav.com>
2020-03-18 13:42:00 +0100

    ArbeidStep.tsx "Er jobben samfunsskritisk" TODO validation

--- src/app/components/søknad/steps/arbeid-step/ArbeidStep.tsx
+++ src/app/components/søknad/steps/arbeid-step/ArbeidStep.tsx
@@ -6,6 +6,11 @@ import { StepConfigProps, StepID } from '../../../../config/stepConfig';
 import { Arbeidssituasjon, SøknadFormField } from '../../../../types/SøknadFormData';
 import { validateArbeid } from '../../../../validation/fieldValidations';
 import FormikStep from '../../formik-step/FormikStep';
+import FormBlock from 'common/components/form-block/FormBlock';
+import TypedFormComponents from '../../typed-form-components/TypedFormComponents';
+import { validateYesOrNoIsAnswered } from 'common/validation/fieldValidations';
+import Lenke from 'nav-frontend-lenker';
+
 
 const ArbeidStep = ({ onValidSubmit }: StepConfigProps) => {
     const intl = useIntl();
@@ -30,6 +35,23 @@ const ArbeidStep = ({ onValidSubmit }: StepConfigProps) => {
                 ]}
                 validate={validateArbeid}
             />
+            <FormBlock>
+                <TypedFormComponents.YesOrNoQuestion
+                    name={SøknadFormField.harSamfunnskritiskJobb}
+                    legend={'Har du en jobb som faller inn under samfunnskritiske funksjoner?'}
+                    validate={validateYesOrNoIsAnswered}
+                    description={
+                        <>
+                            <Lenke
+                                target="_blank"
+                                href="https://www.ks.no/fagomrader/helse-og-omsorg/informasjon-om-koronaviruset/samfunnets-kritiske-funksjoner/">
+                                Se hele listen over jobbene som faller inn samfunnskritiske funksjoner her
+                            </Lenke>
+                            .
+                        </>
+                    }
+                />
+            </FormBlock>
         </FormikStep>
     );
 };
