b03a262dfb4fc8cea1f071662ae4701da18544ac
Are Bjølseth <are.bjolseth@nav.com>
2020-03-25 14:48:11 +0100

    Added e2e director

---
+++ e2e/cypress/integration/inngang.spec.js
@@ -0,0 +1,41 @@
+describe('Overføre Omsorgsdager', () => {
+
+    before(() => {
+        cy.visit('/');
+    });
+    describe('Inngang', () => {
+        it('Startsiden er lastet', () => {
+            cy.location().should((loc) => {
+                expect(loc.pathname).to.eq('/');
+            });
+        });
+        context('Sjekker default verdier i skjema', () => {
+            it('Har Ja/Nei radioknappene for "mottakerErGyldig", som ikke er merket', () => {
+                cy.get('[name="mottakerErGyldig"]')
+                    .parent()
+                    .find('input')
+                    .should('not.be.checked');
+            });
+        });
+        describe('Er den du skal overføre omsorgsdager til arbeidstaker, selvstendig næringsdrivende eller frilanser?', () => {
+
+            context('Ja, skal overføre omsorgsdager', () => {
+                it('Merker "Ja" radio', () => {
+                    cy.get('input[type=radio]').first().click({ force: true });
+                });
+                it('Viser linken "Gå til den digitale søknaden"', () => {
+                    cy.get('a[href*="/familie/sykdom-i-familien/soknad/overfore-omsorgsdager/soknad/velkommen"]')
+                });
+            });
+
+            context('Nei, skal ikke overføre omsorgsdager', () => {
+                it('Merker "Nei" radio', () => {
+                    cy.get('input[type=radio]').last().click({ force: true });
+                });
+                it('Viser info panel"', () => {
+                    cy.get('[class="alertstripe alertstripe--info"]');
+                });
+            });
+        });
+    });
+});
