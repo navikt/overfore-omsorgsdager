109bb61014561764d0801d04f9e8223dfd4cd095
Frode Hansen <frode.hansen@bekk.no>
2020-03-29 20:59:01 +0200

    Test routes

--- src/app/config/routeConfig.ts
+++ src/app/config/routeConfig.ts
@@ -2,11 +2,15 @@ import { getEnvironmentVariable } from 'app/utils/envUtils';
 
 enum RouteConfig {
     UTILGJENGELIG_ROUTE = '/utilgjengelig',
-    APPLICATION_ROUTE_PREFIX = '/melding',
-    ERROR_PAGE_ROUTE = '/melding/feil',
-    WELCOMING_PAGE_ROUTE = '/melding/velkommen',
-    APPLICATION_SENDT_ROUTE = '/melding/melding-sendt'
+    APPLICATION_ROUTE_PREFIX = '/soknad',
+    ERROR_PAGE_ROUTE = '/soknad/feil',
+    WELCOMING_PAGE_ROUTE = '/soknad/velkommen',
+    APPLICATION_SENDT_ROUTE = '/soknad/melding-sendt'
 }
+// SØKNAD_ROUTE_PREFIX = '/soknad',
+// ERROR_PAGE_ROUTE = '/soknad/feil',
+// WELCOMING_PAGE_ROUTE = '/soknad/velkommen',
+// SØKNAD_SENDT_ROUTE = '/soknad/soknad-sendt'
 
 export const getRouteUrl = (route: RouteConfig): string => `${getEnvironmentVariable('PUBLIC_PATH')}${route}`;
 
