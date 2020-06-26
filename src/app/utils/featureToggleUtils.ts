da581b0fba68ed784e325010a759041d0e1b47c8
Frode Hansen <frode.hansen@bekk.no>
2020-06-26 12:15:32 +0200

    Remove unused file

--- src/app/utils/featureToggleUtils.ts
+++
@@ -1,4 +0,0 @@
-export const isFeatureEnabled = (feature: Feature) => {
-    const appSettings = (window as any).appSettings;
-    return appSettings[feature] === 'on' || (window as any).appSettings[feature] === 'true';
-};
