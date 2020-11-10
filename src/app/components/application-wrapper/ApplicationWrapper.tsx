501985d8ea986a7a87a889763bef02103bd9f7b6
Frode Hansen <frode.hansen@bekk.no>
2020-11-10 12:45:18 +0100

    Add languangeToggle

--- src/app/components/application-wrapper/ApplicationWrapper.tsx
+++ src/app/components/application-wrapper/ApplicationWrapper.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import LanguageToggle from '@navikt/sif-common-core/lib/components/language-toggle/LanguageToggle';
 import { Normaltekst } from 'nav-frontend-typografi';
 import ApplicationMessages from 'common/dev-utils/intl/application-messages/ApplicationMessages';
 import { Locale } from 'common/types/Locale';
 import { ApplicantData } from '../../types/ApplicantData';
 import { getEnvironmentVariable } from '../../utils/envUtils';
-import IntlProvider, { appNynorsktekster, appBokmålstekster } from '../intl-provider/IntlProvider';
+import IntlProvider, { appBokmålstekster, appNynorsktekster } from '../intl-provider/IntlProvider';
 
 interface ApplicationWrapperProps {
     søkerdata?: ApplicantData;
@@ -14,9 +15,10 @@ interface ApplicationWrapperProps {
     onChangeLocale: (locale: Locale) => void;
 }
 
-const ApplicationWrapper = ({ locale, children }: ApplicationWrapperProps) => {
+const ApplicationWrapper = ({ locale, onChangeLocale, children }: ApplicationWrapperProps) => {
     return (
         <IntlProvider locale={locale}>
+            <LanguageToggle locale={locale} toggle={onChangeLocale} />
             <Normaltekst tag="div">
                 <Router basename={getEnvironmentVariable('PUBLIC_PATH')}>
                     {children}
